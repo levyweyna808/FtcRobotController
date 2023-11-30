518e8b1837036673744f05ccecf6fb36ac7a946f
Levi Weyna <145080653+levyweyna808@users.noreply.github.com>
2023-11-30 13:07:40 -0700

    Update autoCOdeIStole.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoCOdeIStole.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoCOdeIStole.java
@@ -8,8 +8,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+//commit stuff
 @Autonomous(name="I hate ethan", group="Robot")
 public class autoCOdeIStole extends LinearOpMode {
 
@@ -18,8 +19,11 @@ public class autoCOdeIStole extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private DcMotor linearSlide = null;
+    private Servo grabberServo = null;
     private DcMotor armDrive =null;
     private ElapsedTime runtime = new ElapsedTime();
+    //for the drive train
     static final double COUNTS_PER_MOTOR_REV = 28; // General encoders for all rev motors
     static final double DRIVE_GEAR_REDUCTION = 12.0;
     static final double WHEEL_DIAMETER_INCHES = 3.77953;
@@ -31,6 +35,12 @@ public class autoCOdeIStole extends LinearOpMode {
     static final double SWEEPER_DIAMETER_INCHES = 3;
     static final double COUNTS_PER_SWEEPER_INCH = (COUNTS_PER_MOTOR_REV * SWEEPER_GEARING) / (SWEEPER_DIAMETER_INCHES * 3.1415);
     static final double SPEED_OF_SWEEPER = 1.0;
+    //for linear slide
+    static final double COUNTS_PER_MOTOR_BILDA = 28;
+    static final double LINEAR_SLIDE_GEARING = 	19.20;
+    static final double PULLEY_WHEEL_DIAMETER_INCHES = 1.45;
+    static final double COUNTS_PER_LS_INCH = (COUNTS_PER_MOTOR_BILDA * LINEAR_SLIDE_GEARING) / (PULLEY_WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double LINEAR_SLIDE_SPEED = 1.0;
     @Override
     public void runOpMode() {
 
@@ -40,42 +50,52 @@ public class autoCOdeIStole extends LinearOpMode {
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
         armDrive = hardwareMap.get(DcMotor.class,"Arm_drive" );
+        linearSlide = hardwareMap.get(DcMotor.class,"linear_slide");
+        grabberServo = hardwareMap.get(Servo.class, "box_tilt_servo");
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
         armDrive.setDirection(DcMotor.Direction.REVERSE);
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
+        //servo
+        grabberServo.setDirection(Servo.Direction.FORWARD);
 
         leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at",  "%7d :%7d", leftBackDrive.getCurrentPosition(), leftFrontDrive.getCurrentPosition(),
-        rightBackDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), armDrive.getCurrentPosition());
+        rightBackDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), armDrive.getCurrentPosition(), linearSlide.getCurrentPosition());
         telemetry.update();
 
         waitForStart();
-        encoderDrive(DRIVE_SPEED,  20,  20, 3.0);
-        encoderDrive(TURN_SPEED,   -19.5, 19.5, 3.0);
+        encoderDrive(DRIVE_SPEED,  20,  20, 3.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   -19.5, 19.5, 3.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         //SweeperDrive(SPEED_OF_SWEEPER, 0.3, 3.0);
+        LinearSlideDrive(LINEAR_SLIDE_SPEED, 0.4, 3.0);
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(1000);
+        sleep(1000);  // pause to display final telemetry message.
     }
     public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS){
         int newLeftFrontTarget;
@@ -84,6 +104,7 @@ public class autoCOdeIStole extends LinearOpMode {
         int newRightBackTarget;
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
+
             // Determine new target position, and pass to motor controller
             newLeftFrontTarget = leftFrontDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
             newRightFrontTarget = rightFrontDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
@@ -98,6 +119,7 @@ public class autoCOdeIStole extends LinearOpMode {
             rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
             // reset the timeout time and start motion.
             runtime.reset();
             leftFrontDrive.setPower(Math.abs(speed));
@@ -107,12 +129,14 @@ public class autoCOdeIStole extends LinearOpMode {
             while (opModeIsActive() &&
                    (runtime.seconds() < timeoutS) &&
                    (leftFrontDrive.isBusy() && rightFrontDrive.isBusy() && leftBackDrive.isBusy() && rightBackDrive.isBusy())) {
+
                 // Display it for the driver.
 //                telemetry.addData("Running to",  " %7d :%7d", leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive);
 //                telemetry.addData("Currently at",  " at %7d :%7d", leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(),
 //                        leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
                 telemetry.update();
             }
+
             // Stop all motion;
             leftFrontDrive.setPower(0);
             leftBackDrive.setPower(0);
@@ -131,6 +155,7 @@ public class autoCOdeIStole extends LinearOpMode {
         int sweeperTarget;
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
+
             // Determine new target position, and pass to motor controller
             sweeperTarget = armDrive.getCurrentPosition() + (int)(sweeperRotationInches * COUNTS_PER_SWEEPER_INCH);
             armDrive.setTargetPosition(sweeperTarget);
@@ -140,6 +165,7 @@ public class autoCOdeIStole extends LinearOpMode {
             runtime.reset();
             armDrive.setPower(Math.abs(sweeperSpeed));
             while (opModeIsActive() && (runtime.seconds() < SweeperTmeoutS) && (armDrive.isBusy())) {
+
                 // Display it for the driver.
 //                telemetry.addData("Running to",  " %7d :%7d", leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive);
 //                telemetry.addData("Currently at",  " at %7d :%7d", leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(),
@@ -150,6 +176,35 @@ public class autoCOdeIStole extends LinearOpMode {
             armDrive.setPower(0);
             // Turn off RUN_TO_POSITION
             armDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            sleep(250);   // optional pause after each move.
+        }
+    }
+
+    public void LinearSlideDrive(double slideSpeed, double slideRotationInches, double slideTmeoutS){
+        int slideTarget;
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            slideTarget = linearSlide.getCurrentPosition() + (int)(slideRotationInches * COUNTS_PER_LS_INCH);
+            linearSlide.setTargetPosition(slideTarget);
+            // Turn On RUN_TO_POSITION
+            linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            // reset the timeout time and start motion.
+            runtime.reset();
+            linearSlide.setPower(Math.abs(slideSpeed));
+            while (opModeIsActive() && (runtime.seconds() < slideTmeoutS) && (linearSlide.isBusy())) {
+
+                // Display it for the driver.
+//                telemetry.addData("Running to",  " %7d :%7d", leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive);
+//                telemetry.addData("Currently at",  " at %7d :%7d", leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(),
+//                        leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+                telemetry.update();
+            }
+            // Stop all motion;
+            linearSlide.setPower(0);
+            // Turn off RUN_TO_POSITION
+            linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             sleep(250);   // optional pause after each move.
         }
     }
